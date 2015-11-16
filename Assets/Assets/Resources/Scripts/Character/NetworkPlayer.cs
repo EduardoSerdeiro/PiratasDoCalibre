2931f9b3ffc357b32484a374e910a49353b0705e
EduardoSerdeiro <eduardoassislima@hotmail.com>
2015-11-16 14:52:05 -0200

    networkplayer++

---
+++ Assets/Assets/Resources/Scripts/Character/NetworkPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+    private Vector3 corretPlayerPos = Vector3.zero;
+    private Quaternion corretPlayerRot = Quaternion.identity;
+
+	
+	void Update () {
+
+        if (!photonView.isMine)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, this.corretPlayerPos, Time.deltaTime * 5);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, this.corretPlayerRot, Time.deltaTime * 5);
+        }
+
+	}
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(this.transform);
+            stream.SendNext(this.transform.rotation);
+        }
+        else
+        {
+            this.corretPlayerPos = (Vector3)stream.ReceiveNext();
+            this.corretPlayerRot = (Quaternion)stream.ReceiveNext();
+
+
+        }
+    }
+
+
+}
---
+++ Assets/Assets/Resources/Scripts/Utilitys/Vazio.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vazio : MonoBehaviour
+{
+
+    void Start()
+    {
+      //  PhotonNetwork.ConnectUsingSettings("0.1");
+      //  RoomOptions options = new RoomOptions();
+      //  options.maxPlayers = 4;
+        //peer.OpCreateRoom("Room 42", options, TypedLobby.Default);
+       // OnConnectedToMaster();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+
+    //public void OnConnectedToMaster()
+    //{
+    //    Debug.Log("asda");
+    //    PhotonNetwork.JoinRandomRoom();
+    //}
+    //void OnPhotonRandomJoinFailed()
+    //{
+    //    Debug.Log("OnPhotonRandomJoinFailed");
+    //    PhotonNetwork.CreateRoom(null);
+    //}
+}
+	
