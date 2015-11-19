a76a3a5da84a591a3b02f7bbe37c999888fea4b5
EduardoSerdeiro <eduardoassislima@hotmail.com>
2015-11-19 01:53:16 -0200

    ping++ fps++

---
+++ Assets/Assets/Resources/Scripts/Utilitys/FpsGame.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FpsGame : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Fps;
+
+    private Text TextFps;
+
+    private int fpsAccumulator = 0;
+    private float fpsNextPeriod = 0;
+    private int currentFps;
+    const float fpsPeriod = 0.5f;
+
+
+	void Start () {
+        
+        Fps.SetActive(false);
+        fpsNextPeriod = Time.realtimeSinceStartup + fpsPeriod;
+        TextFps = this.transform.GetChild(2).GetComponent<Text>();
+	}
+	
+	void Update () 
+    {
+        fpsAccumulator++;
+        if(Time.realtimeSinceStartup > fpsNextPeriod)
+        {
+            currentFps = (int)(fpsAccumulator / fpsPeriod);
+            fpsAccumulator = 0;
+            fpsNextPeriod += fpsPeriod;
+            TextFps.text = string.Format("{0} FPS", currentFps);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            Fps.SetActive(!Fps.activeSelf);
+
+        }
+        
+        //Debug.Log(this.gameObject.activeSelf);
+
+	}
+}
---
+++ Assets/Assets/Resources/Scripts/Utilitys/PingGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PingGame : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Ping;
+
+    private Text TextPing;
+
+	void Start () {
+
+        Ping.SetActive(false);
+        TextPing = this.transform.GetChild(3).GetComponent<Text>();
+
+	}
+	
+	void Update () {
+
+        TextPing.text = "Ping: " + PhotonNetwork.GetPing().ToString();
+
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            Ping.SetActive(!Ping.activeSelf);
+
+        }
+
+	}
+}
