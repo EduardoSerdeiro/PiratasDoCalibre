194e797de9df4b3e897abfcef8c02ae7fc622c29
EduardoSerdeiro <eduardoassislima@hotmail.com>
2015-11-25 18:07:31 -0200

    Script atq 3º tentativa

---
+++ Assets/Assets/Resources/Scripts/Character/AtqCorpo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtqCorpo : MonoBehaviour {
+
+    [Header("Atributos Golpear")]
+    [SerializeField]private float damageGolpe;
+    [SerializeField]
+    private float cooldownGolpe;
+
+    private float timeGolpe;
+
+	void Start () {
+
+        timeGolpe = 0;
+
+	}
+	
+	void Update () {
+
+        timeGolpe += Time.deltaTime;
+
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if (Input.GetMouseButtonDown(1) && timeGolpe >= cooldownGolpe)
+            {
+                GameObject inimigo = col.gameObject;
+                inimigo.GetComponent<Health>().TakeDamage(damageGolpe);
+                timeGolpe = 0;
+            }
+
+        }
+
+    }
+}
