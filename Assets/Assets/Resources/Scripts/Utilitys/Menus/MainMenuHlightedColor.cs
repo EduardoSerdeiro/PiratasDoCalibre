26386b0433d952c977c268fc3b2a3fd6b4427833
EduardoSerdeiro <eduardoassislima@hotmail.com>
2015-12-16 22:12:20 -0200

    interfaces++

---
+++ Assets/Assets/Resources/Scripts/Utilitys/Menus/MainMenuHlightedColor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MainMenuHlightedColor : MonoBehaviour
+{
+    public Button btnJogar;
+    public Text textJogar;
+
+    public Text textConfig;
+    public Text textCredits;
+    public Text textSair;
+
+    public void TxtJgarColorEnter()
+    {
+        textJogar.color = Color.Lerp(Color.red, Color.yellow, 0.3f);
+    }
+    public void TxtJgarColorExit()
+    {
+        textJogar.color = Color.white;
+    }
+
+
+    public void TxtConfigColorEnter()
+    {
+        textConfig.color = Color.Lerp(Color.red, Color.yellow, 0.3f);
+    }
+    public void TxtConfigColorExit()
+    {
+        textConfig.color = Color.white;
+    }
+
+    public void TxtCreditColorEnter()
+    {
+        textCredits.color = Color.Lerp(Color.red, Color.yellow, 0.3f);
+    }
+    public void TxtCreditColorExit()
+    {
+        textCredits.color = Color.white;
+    }
+
+    public void TxtQuitColorEnter()
+    {
+        textSair.color = Color.Lerp(Color.red, Color.yellow, 0.3f);
+    }
+    public void TxtQuitColorExit()
+    {
+        textSair.color = Color.white;
+    }
+
+}
